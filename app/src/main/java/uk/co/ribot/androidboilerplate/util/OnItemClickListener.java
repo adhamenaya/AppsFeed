62c318a9b6c26e9873ea8533c8c12344561ee06f
adhamenaya <a.it@hotmail.com>
2016-04-10 23:41:51 +0300

    remove old files

--- app/src/main/java/uk/co/ribot/androidboilerplate/util/OnItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package uk.co.ribot.androidboilerplate.util;
-
-/**
- * Created by adhamenaya on 4/9/2016.
- */
-public interface OnItemClickListener {
-
-    public void onItemClick(Object object, int position);
-}
